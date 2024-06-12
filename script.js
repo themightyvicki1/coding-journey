ba8c7a9d09f71ccf947094cc7304fe74659b2957
themightyvicki1 <victoria.greer861@gmail.com>
2024-06-12 16:30:12 -0700

    added a little javascript code

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("Hello");
+function welcomeMessage() {
+  alert("Welcome to my journey!");
+}
+let button = document.querySelector("button");
+button.addEventListener("click", welcomeMessage);
